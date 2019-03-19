9e9cf09c7877471a0faa2597ef486dc578b73db0
khalilayache <khalilayache@gmail.com>
2019-03-19 17:46:05 -0300

    Java Exercises resolutions

---
+++ src/jv/output_formatting/Solution.java
@@ -0,0 +1,23 @@
+package jv.output_formatting;
+
+import java.util.Scanner;
+
+//https://www.hackerrank.com/challenges/java-output-formatting/problem
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("================================");
+        for (int i = 0; i < 3; i++) {
+            String s1 = sc.next();
+            int x = sc.nextInt();
+            for(int j =  s1.length() ; j < 15 ; j++){
+                s1 = s1.concat(" ");
+            }
+            System.out.println(s1 + String.format("%03d", x));
+        }
+        System.out.println("================================");
+    }
+}
---
+++ src/jv/stdin_stdout_ii/Solution.java
@@ -0,0 +1,43 @@
+package jv.stdin_stdout_ii;
+
+import java.util.Scanner;
+
+//https://www.hackerrank.com/challenges/java-stdin-stdout/problem
+
+/*this challenge should be bugged on Hackerrank site cause this code
+    executed on site cause error in 2 test cases
+    but executing here the output is the same on
+    test cases errors
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+//        int i = scan.nextInt();
+//        double d = scan.nextDouble();
+//        String s = scan.next();
+//
+//        while (scan.hasNext()) {
+//            s = s.concat(" " + scan.next());
+//        }
+
+        int i = 2147483647;
+        double d = 235345345345.234534;
+        String s = "   fsdfsdf   sdf";
+
+        System.out.println("String: " + s.trim());
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+
+
+        i = -23123;
+        d = 123123232;
+        s = "  m";
+
+        System.out.println("\n");
+        System.out.println("String: " + s.trim());
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
