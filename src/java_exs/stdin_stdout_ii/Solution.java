0bf547532f03d7928c71751da706fc316a93575e
khalilayache <khalilayache@gmail.com>
2019-03-26 13:53:02 -0300

    Fix Stdin Stdout II exercise resolution

--- src/java_exs/stdin_stdout_ii/Solution.java
+++ src/java_exs/stdin_stdout_ii/Solution.java
@@ -1,42 +1,18 @@
 package java_exs.stdin_stdout_ii;
 
-import java.util.Scanner;
-
-//https://www.hackerrank.com/challenges/java-stdin-stdout/problem
-
-/*this challenge should be bugged on Hackerrank site cause this code
-    executed on site cause error in 2 test cases
-    but executing here the output is the same on
-    test cases errors
-*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-//        int i = scan.nextInt();
-//        double d = scan.nextDouble();
-//        String s = scan.next();
-//
-//        while (scan.hasNext()) {
-//            s = s.concat(" " + scan.next());
-//        }
+    public static void main(String[] args) throws IOException {
+        BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
+        int i = Integer.parseInt(scan.readLine());
+        double d = Double.parseDouble(scan.readLine());
+        String s = scan.readLine();
 
-        int i = 2147483647;
-        double d = 235345345345.234534;
-        String s = "   fsdfsdf   sdf";
-
-        System.out.println("String: " + s.trim());
-        System.out.println("Double: " + d);
-        System.out.println("Int: " + i);
-
-
-        i = -23123;
-        d = 123123232;
-        s = "  m";
-
-        System.out.println("\n");
-        System.out.println("String: " + s.trim());
+        System.out.println("String: " + s);
         System.out.println("Double: " + d);
         System.out.println("Int: " + i);
     }
